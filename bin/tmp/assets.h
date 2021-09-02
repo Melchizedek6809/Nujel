439d4fb13b25648e55455ab32158b7d27c4be3c6
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-09-03 00:17:13 +0200

    Removed errneously commited temporary files

--- bin/tmp/assets.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stddef.h>
-
-extern unsigned  int tmp_saolib_nuj_len;
-extern unsigned char tmp_saolib_nuj_data[];
-
-extern unsigned  int src_tmp_stdlib_nuj_len;
-extern unsigned char src_tmp_stdlib_nuj_data[];
-
