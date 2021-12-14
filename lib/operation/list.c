08c07d73d2359ad009da7eb0ef41d44c5192794b
Ben (Win10) <ben@wolkenwelten.net>
2021-12-14 17:13:47 +0100

    Added nreverse and a faster map

--- lib/operation/list.c
+++ lib/operation/list.c
@@ -4,6 +4,7 @@
  * This project uses the MIT license, a copy should be included under /LICENSE
  */
 #include "list.h"
+#include "../display.h"
 #include "../exception.h"
 #include "../nujel.h"
 #include "../collection/list.h"
@@ -28,8 +29,21 @@ static lVal *lnfCons(lClosure *c, lVal *v){
 	return lCons(lCar(v),lCadr(v));
 }
 
+static lVal *lnfNReverse(lClosure *c, lVal *v){
+	(void)c;
+	lVal *t = NULL, *l = lCar(v);
+	while((l != NULL) && (l->type == ltPair)){
+		lVal *next = l->vList.cdr;
+		l->vList.cdr = t;
+		t = l;
+		l = next;
+	}
+	return t;
+}
+
 void lOperationsList(lClosure *c){
 	lAddNativeFunc(c,"car",  "[list]",     "Returs the head of LIST",          lnfCar);
 	lAddNativeFunc(c,"cdr",  "[list]",     "Return the rest of LIST",          lnfCdr);
 	lAddNativeFunc(c,"cons", "[car cdr]",  "Return a new pair of CAR and CDR", lnfCons);
+	lAddNativeFunc(c,"nreverse","[list]",  "Return LIST in reverse order, fast but mutates", lnfNReverse);
 }
