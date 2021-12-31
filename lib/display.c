ce090ddce478692ab8f5c71d19e038641eb50e3a
Ben (X13/Void) <ben@wolkenwelten.net>
2021-12-31 01:23:30 +0100

    Better wasm error/console output

--- lib/display.c
+++ lib/display.c
@@ -8,9 +8,24 @@
 
 char dispWriteBuf[1<<18];
 
+#ifdef __EMSCRIPTEN__
+	#include <emscripten.h>
+
+	EM_JS(void, wasmConsoleLog, (const char *str), {
+		console.log(UTF8ToString(str));
+	});
+
+	EM_JS(void, wasmConsoleError, (const char *str), {
+		console.error(UTF8ToString(str));
+	});
+#endif
+
 /* Display v on the default channel, most likely stdout */
 void lDisplayVal(lVal *v){
 	char *end = spf(dispWriteBuf,&dispWriteBuf[sizeof(dispWriteBuf)],"%v",v);
+	#ifdef __EMSCRIPTEN__
+	wasmConsoleLog(dispWriteBuf);
+	#endif
 	fwrite(dispWriteBuf, end - dispWriteBuf, 1, stdout);
 }
 
@@ -23,11 +38,17 @@ const char *lReturnDisplayVal(lVal *v){
 /* Display v on the error channel, most likely stderr */
 void lDisplayErrorVal(lVal *v){
 	char *end = spf(dispWriteBuf,&dispWriteBuf[sizeof(dispWriteBuf)],"%V",v);
+	#ifdef __EMSCRIPTEN__
+	wasmConsoleError(dispWriteBuf);
+	#endif
 	fwrite(dispWriteBuf, end - dispWriteBuf, 1, stderr);
 }
 
 /* Write a machine-readable presentation of v to stdout */
 void lWriteVal(lVal *v){
 	char *end = spf(dispWriteBuf,&dispWriteBuf[sizeof(dispWriteBuf)],"%V",v);
+	#ifdef __EMSCRIPTEN__
+	wasmConsoleLog(dispWriteBuf);
+	#endif
 	fwrite(dispWriteBuf, end - dispWriteBuf, 1, stdout);
 }
