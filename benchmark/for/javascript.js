d7de178ed92896183d234f49cbcdf3860402f190
Ben (X13/Arch) <ben@wolkenwelten.net>
2022-05-20 16:55:36 +0200

    Faster JS benchmark

--- benchmark/for/javascript.js
+++ benchmark/for/javascript.js
@@ -4,8 +4,11 @@ try{
     var consoleLog = console.log;
 } // mujs workaround
 
-var ret = 0;
-for(var i=0;i<10000000;i++){
-    ret += i;
+function testRun(){
+    var ret = 0;
+    for(var i=0;i<10000000;i++){
+        ret += i;
+    }
+    consoleLog("The result is: " + ret);
 }
-consoleLog("The result is: " + ret);
+testRun();
