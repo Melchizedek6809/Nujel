706e8b98fe5272017a112273380da406c4b38f32
Ben (Win10) <ben@wolkenwelten.net>
2022-07-27 21:12:17 +0200

    GH CI Valgrind now tracks the origin

--- mk/targets.mk
+++ mk/targets.mk
@@ -83,7 +83,7 @@ fuzz: $(FUZZ_NUJEL)
 	@$(AFL_FUZZ) -i tests/fuzz/ -o /tmp/wwfuzz -m 128 -t 10000 -d -- ./$(FUZZ_NUJEL) @@
 
 valgrind: $(NUJEL)
-	valgrind --error-exitcode=1 ./$(NUJEL) tools/tests.nuj
+	valgrind --error-exitcode=1 --track-origins=yes ./$(NUJEL) tools/tests.nuj
 
 profile: $(NUJEL)
 	valgrind --tool=callgrind --dump-instr=yes ./$(NUJEL) --only-test-suite tools/tests.nuj
