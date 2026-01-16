d4fa83df1ffbece0fca76dbdc27197525ad2030a
Ben (X13/Arch) <ben@wolkenwelten.net>
2026-01-16 04:13:28 +0000

    NixOS flake dev shell

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Nujel";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Target system(s) — adjust if you need macOS or other
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+      {
+        devShells = forAllSystems (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+            {
+              default = pkgs.mkShell {
+                strictDeps = true;
+                nativeBuildInputs = with pkgs; [
+                  gcc
+                  clang
+                  mold
+                  dash
+                  gnumake
+                ];
+              };
+            });
+      };
+}
